0ad56de9f90045967586bf78d5db45fc8b36896f
Jen <jhasenau@greenpeace.org>
2020-04-22 14:31:17 +1000

    change create to apply

--- create_clusterissuer.sh
+++ create_clusterissuer.sh
@@ -1,23 +1,8 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
+#Create ClusterIssuer to allow cert maintenance across namespaces
+# Refer to https://cert-manager.io/docs/tutorials/acme/ingress/
 
-CISSUER_STAGE_COUNT=$(kubectl get ClusterIssuer -o jsonpath='{@}' | grep -c letsencrypt-staging)
-CISSUER_PROD_COUNT=$(kubectl get ClusterIssuer -o jsonpath='{@}' | grep -c letsencrypt-prod)
-
-echo "Count Stagine Cluster Issuers: ${CISSUER_STAGE_COUNT}"
-if (( CISSUER_STAGE_COUNT > 0 ))
-then
-  echo "Stage Cluster Issuer already exists"
-else
-  echo "Stage Cluster Issuer does not exist, creating it now"
-  kubectl create -f letsencrypt-staging.yaml
-fi
-
-echo "Count Prod Cluster Issuers: ${CISSUER_PROD_COUNT}"
-if (( CISSUER_PROD_COUNT > 0 ))
-then
-  echo "Prod Cluster Issuer already exists"
-else
-  echo "Prod Cluster Issuer does not exist, creating it now"
-  kubectl create -f letsencrypt-prod.yaml
-fi
+  kubectl apply -f letsencrypt-staging.yaml
+  kubectl apply -f letsencrypt-prod.yaml
