f44ed48512d3598d2b8e75c8dd83d0ca382dbe9e
gmintoco <50625416+gmintoco@users.noreply.github.com>
2022-08-31 11:40:37 +1000

    Update cert manager version (#12)
    * Update cert manager version
    
    * Update create_crds.sh

--- create_crds.sh
+++ create_crds.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 #Install the CustomResourceDefinition resources first separately.
 #https://cert-manager.io/docs/installation/kubernetes/#installing-with-helm
 #Kubernetes 1.15+
-  kubectl apply --validate=false \
-	 -f https://github.com/jetstack/cert-manager/releases/download/v1.4.0/cert-manager.crds.yaml
+  kubectl create --validate=false \
+	 -f https://github.com/cert-manager/cert-manager/releases/download/v1.9.1/cert-manager.crds.yaml
