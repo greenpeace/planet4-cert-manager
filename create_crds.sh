12c8a57d960d59416d002e97c69d529ff83454ab
Gus Minto-Cowcher <gmintoco@greenpeace.org>
2022-08-31 12:11:42 +1000

    fix: create CRDS

--- create_crds.sh
+++ create_crds.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 #Install the CustomResourceDefinition resources first separately.
 #https://cert-manager.io/docs/installation/kubernetes/#installing-with-helm
 #Kubernetes 1.15+
-  kubectl replace --validate=false \
+  kubectl apply --validate=false \
 	 -f https://github.com/cert-manager/cert-manager/releases/download/v1.7.3/cert-manager.crds.yaml
