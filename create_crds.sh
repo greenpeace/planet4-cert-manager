2ae1a97d5d6f6f7f51003bd8303c5213eb479ed3
gmintoco <50625416+gmintoco@users.noreply.github.com>
2022-08-31 11:46:55 +1000

    Update create_crds.sh

--- create_crds.sh
+++ create_crds.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 #https://cert-manager.io/docs/installation/kubernetes/#installing-with-helm
 #Kubernetes 1.15+
   kubectl replace --validate=false \
-	 -f https://github.com/cert-manager/cert-manager/releases/download/v1.6.0/cert-manager.crds.yaml
+	 -f https://github.com/cert-manager/cert-manager/releases/download/v1.6.3/cert-manager.crds.yaml
